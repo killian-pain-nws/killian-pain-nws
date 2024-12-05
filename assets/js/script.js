536f6f73fca9f9a90cbf1c8633d22147c5056123
Killian <painkillian76@gmail.com>
2024-12-05 16:52:30 +0100

    Code review

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log("Bienvenue sur la page d'exemple !");
+/**
+ * Scroll Indicator Progress Bar
+ */
+const handleScrollIndicator = () => {
+	const scrollIndicator = document.querySelector("#scroll-indicator");
+	const maxHeight = document.body.scrollHeight - window.innerHeight;
+
+	const widthPercentage = (window.scrollY / maxHeight) * 100;
+	scrollIndicator.style.width = `${widthPercentage}%`;
+};
+
+window.addEventListener("scroll", handleScrollIndicator);
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
